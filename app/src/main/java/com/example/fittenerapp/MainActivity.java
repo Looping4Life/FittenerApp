2bc86bec5f214193df62403ba08130a2a8027579
Janne Kaukua <Janne.markus@hotmail.fi>
2020-04-28 15:20:31 +0300

    Main xml update

--- app/src/main/java/com/example/fittenerapp/MainActivity.java
+++ app/src/main/java/com/example/fittenerapp/MainActivity.java
@@ -10,13 +10,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.view.View;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private static final String PREFS = "SavedValues";
     private static final String LISTSIZE = "List size";
-    ListHolder lh;
+    private ListHolder lh;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +32,23 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences pref = getSharedPreferences(PREFS, Activity.MODE_PRIVATE);
         lh = ListHolder.getInstance();
         int listSize = pref.getInt(LISTSIZE, 0);
-        for (int i = 0; i < listSize; i++) {
-            String s = Integer.toString(i);
-            lh.getEntryList().add(new Entry(pref.getString(s, ""), pref.getFloat(s, 0), pref.getInt(s, 0)));
+        if(listSize > 0) {
+            for (int i = 0; i < listSize; i++) {
+                String s = Integer.toString(i);
+                lh.getEntryList().add(new Entry(pref.getString("entrydata " + s, ""), pref.getFloat("entrydata " + s, 0), pref.getInt("entrydata " + s, 0)));
+            }
         }
-
+        editText = findViewById(R.id.editText);
     }
 
     public void ButtonPressed(View view){
-        Intent intent = new Intent(MainActivity.this, Calendar.class);//TODO: Update list instead
-        startActivity(intent);
+        if(view == findViewById(R.id.add_entry)){
+            lh.getEntryList().add(new Entry(new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date()), Float.parseFloat(editText.getText().toString()), 180));
+        }
+        if(view == findViewById(R.id.calendar)){
+            Intent intent = new Intent(MainActivity.this, Calendar.class);//TODO: Update list instead
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -44,15 +57,18 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences pref = getSharedPreferences(PREFS, Activity.MODE_PRIVATE);
         SharedPreferences.Editor edit = pref.edit();
         int listSize = lh.getEntryList().size();
-        for (int i = 0; i < listSize; i++) {
-            Entry entry = lh.getEntryList().get(i);
-            edit.putString(Integer.toString(i), entry.toString());
-            edit.putFloat(Integer.toString(i), entry.weight);
-            edit.putInt(Integer.toString(i), entry.height);
+        if(listSize > 0){
+            for (int i = 0; i < listSize; i++) {
+                Entry entry = lh.getEntryList().get(i);
+                String s = Integer.toString(i);
+                edit.putString("entrydata " + s, entry.toString());
+                edit.putFloat("entrydata " + s, entry.weight);
+                edit.putInt("entrydata " + s, entry.height);
 
+            }
+            edit.putInt(LISTSIZE, listSize);
+            edit.commit();
         }
-        edit.putInt(LISTSIZE, listSize);
-        edit.apply();
     }
 
     @Override
