b98afa3c34c6bd22d5f52c5c3c8af8552e942414
Sasha <alexander.sanmiguel@metropolia.fi>
2020-04-30 07:37:26 +0300

    Confirmation pop-up now resets the list upon user confirmation. Tested - works!

--- app/src/main/java/com/example/fittenerapp/SettingsActivity.java
+++ app/src/main/java/com/example/fittenerapp/SettingsActivity.java
@@ -1,38 +1,53 @@
 package com.example.fittenerapp;
 
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Class to implement a Settings activity
+ * @author Alexander San Miguel
+ * @version 0.1 29/4/2020
+ */
+
 public class SettingsActivity extends AppCompatActivity {
     private static final String PREFS = "SavedValues";
 
+    private ListHolder lh;
+    private EditText editText;
     Button resetButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_layout);
+        resetButton = findViewById(R.id.resetButton);
 
-        resetButton = (Button) findViewById(R.id.resetButton);
 
-        resetButton.setOnClickListener(new View.OnClickListener(){
+        resetButton.setOnClickListener(new View.OnClickListener(){ //Draws an alert dialog to request confirmation of history deletion
             @Override
             public void onClick(View v){
                 AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
 
-                builder.setMessage("Are you sure you want to delete your history?")
-                        .setPositiveButton("Ye do it", new DialogInterface.OnClickListener(){
+                builder.setMessage("You are about to delete everything.\nAre you sure?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener(){
                             @Override
                             public void onClick(DialogInterface dialog, int which){
-                                // Here is where we gotta delete the thing//
+                                SharedPreferences pref = getSharedPreferences(PREFS, Activity.MODE_PRIVATE);
+                                SharedPreferences.Editor edit = pref.edit();
+                                edit.clear();
+                                edit.commit();
+                                lh.getEntryList().clear();
+
                             }
-                        }).setNegativeButton("Na I'm a wuss", null);
+                        }).setNegativeButton("No", null);
                 AlertDialog alert = builder.create();
                 alert.show();
             }
